826241976cd4461be32850cb57857b27de5c78f9
Ryan Volz <iggyvolz@gmail.com>
2017-12-04 19:32:55 -0500

    Add updated sheet music program

--- sheetmusic.js
+++ sheetmusic.js
@@ -10,7 +10,7 @@ $(function()
     // Draw background
     const background = svg.rect(WIDTH,HEIGHT).fill(BACKGROUND_COLOUR);
     // Draw staff
-    drawStaff(svg, 50,50,100,50,2);
+    drawStaff(svg, 50,50,100,200,2);
     // https://stackoverflow.com/a/42711775
     var pt = svg.node.createSVGPoint();
     function localClickCoordinates(e)
@@ -27,8 +27,9 @@ $(function()
         for(var i=0;i<=4;i++)
         {
             // Draw a line of the staff
-            // Go by 1/4th of height-stroke, so we start at the top and end at the bottom (less the stroke)
-            svg.line(left, top+i*(height-stroke)/4, left+width, top+i*(height-stroke)/4).stroke({width:stroke, color: STAFF_COLOUR});
+            // Go by 1/4.5th of height (plus 1/18th), so we start 1/18th from the top and end 1/18th from the bottom 
+            // We also need to subtract half of the stroke, so that the line is centred at the point we want it
+            svg.line(left, top+i*height/4.5+(height/18)-stroke/2, left+width, top+i*height/4.5+(height/18)-stroke/2).stroke({width:stroke, color: STAFF_COLOUR});
         }
         svg.click(function(e)
         {
@@ -42,7 +43,22 @@ $(function()
                 // Ignore click, it was not meant for us
                 return;
             }
-            console.log("Ouch!");
+            // Calculate line/space on which we clicked
+            // Each line/space has a buffer of 1/9th of the staff, so multiply by 9 and get the floor
+            const clickedSpot=Math.floor(coordinates.y/height*9);
+            // Place a quarter note at that spot
+            const ellipseHeight=10;
+            const ellipseWidth=15;
+            svg.ellipse(ellipseWidth, ellipseHeight).move(left + coordinates.x - ellipseWidth/2, top + height*(clickedSpot + 1/2)/9-stroke/2-ellipseHeight/2);
+            // Draw the stem
+            if(clickedSpot<=4)
+            {
+                // TODO Draw stem pointing down
+            }
+            else
+            {
+                // TODO Draw stem pointing up
+            }
         });
     }
 });
