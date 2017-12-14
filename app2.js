9be249ae4eea6943c9d95cbbae0294ac60c89ecd
Ryan "iggy" Volz <iggyvolz@gmail.com>
2017-12-14 16:30:53 -0500

    Move drawing notes and clefs to their own functions

--- app2.js
+++ app2.js
@@ -24,86 +24,21 @@ $(function()
         // Translate into svg coordinates
         return pt.matrixTransform(svg.node.getScreenCTM().inverse());
     }
-    /**
-     * Draws a staff
-     * @param {SVG} svg SVG.js object
-     * @param {number} left Left border of the staff
-     * @param {number} top Top border of the staff
-     * @param {int} width Width of the staff
-     * @param {int} height Height of the staff
-     * @param {int} stroke Line stroke of the staff
-     * @param {string} clef Type of clef to draw (treble/bass/none)
-     */
-    function drawStaff(svg, left, top, width, height, stroke, clef)
+    function drawClef(svg, staff, clef)
     {
-        // Draw staff
-        // Save the line numbers so we can reference their positions later
-        const lines=[];
-        for(var i=0;i<=4;i++)
-        {
-            // Draw a line of the staff
-            // Go by 1/4.5th of height (plus 1/18th), so we start 1/18th from the top and end 1/18th from the bottom 
-            // We also need to subtract half of the stroke, so that the line is centred at the point we want it
-            lines.push(top+i*height/4.5+(height/18)-stroke/2);
-            svg.line(left, lines[i], left+width, lines[i]).stroke({width:stroke, color: STAFF_COLOUR});
-        }
-        svg.click(function(e)
-        {
-            // Get click coordinates relative to the svg
-            const clickCoordinates = localClickCoordinates(e);
-            console.log(clickCoordinates);
-            // Get coordinates relative to the staff
-            clickCoordinates.x-=left;
-            clickCoordinates.y-=top;
-            if(clickCoordinates.x<0 || clickCoordinates.x>width || clickCoordinates.y<0 || clickCoordinates.y>height)
-            {
-                // Ignore click, it was not meant for us
-                return;
-            }
-            // Calculate line/space on which we clicked
-            // Each line/space has a buffer of 1/9th of the staff, so multiply by 9 and get the floor
-            const clickedSpot=Math.floor(clickCoordinates.y/height*9);
-            // Place a quarter note at that spot
-            const ellipseHeight=10;
-            const ellipseWidth=15;
 
-            // SVG's move wants the top-left of the rectangle containing the ellipse
-            const ellipseLeft = left + clickCoordinates.x - ellipseWidth/2;
-            const ellipseTop = top + height*(clickedSpot + 1/2)/9-stroke/2-ellipseHeight/2;
-            svg.ellipse(ellipseWidth, ellipseHeight).move(ellipseLeft, ellipseTop);
-            // Draw the stem
-            if(clickedSpot<=4)
-            {
-                // Draw stem pointing down
-                // Stem starts at the left of the ellipse and goes down
-                const stemTop = ellipseTop + ellipseHeight/2; // Vertical centre of the ellipse
-                const stemBottom = ellipseTop + ellipseHeight/2 + STEM_HEIGHT;
-                const stemX = ellipseLeft + ellipseWidth*.1; // Put the stem a little into the note so it isn't as obvious
-                svg.line(stemX, stemTop, stemX, stemBottom).stroke({width:STEM_STROKE, color: STEM_COLOUR});
-            }
-            else
-            {
-                // Draw stem pointing up
-                // Stem starts at the right of the ellipse and goes up
-                const stemTop = ellipseTop + ellipseHeight/2 - STEM_HEIGHT; // Vertical centre of the ellipse
-                const stemBottom = ellipseTop + ellipseHeight/2;
-                const stemX = ellipseLeft + ellipseWidth*.9; // Put the stem a little into the note so it isn't as obvious
-                svg.line(stemX, stemTop, stemX, stemBottom).stroke({width:STEM_STROKE, color: STEM_COLOUR});
-            }
-        });
-        // Draw the clef
         switch(clef)
         {
             case "treble":
                 // Draw a treble clef
                 // Calculate distance between lines
-                const distanceBetweenLines=lines[1]-lines[0];
+                const distanceBetweenLines=staff.lines[1]-staff.lines[0];
                 // Horizontally, start 2/3ds of the way into the clef
                 const x = CLEF_LEFT+CLEF_WIDTH*2/3;
                 // Vertically, start one third of a line length above top, end two thirds of a line length below the bottom
-                svg.line(x,top-distanceBetweenLines/3,x,lines[4]+distanceBetweenLines*2/3).stroke({width:STEM_STROKE, color: STEM_COLOUR});
+                svg.line(x,staff.top-distanceBetweenLines/3,x,staff.lines[4]+distanceBetweenLines*2/3).stroke({width:STEM_STROKE, color: STEM_COLOUR});
                 // Now make the curve at the bottom
-                const y = lines[4]+distanceBetweenLines*2/3;
+                const y = staff.lines[4]+distanceBetweenLines*2/3;
 
                 // X-radius should be 1/3rd of a clef (so that the diameter is 2/3ds of a clef,
                 // taking us from the main stem to the left edge of the clef)
@@ -123,7 +58,7 @@ $(function()
                 // Draw arc from above line to D line
                 // Start at last time's final position
                 const x2=x;
-                const y2=lines[0]-distanceBetweenLines/2;
+                const y2=staff.lines[0]-distanceBetweenLines/2;
                 const rx2=CLEF_WIDTH*2/3;
                 const ry2=distanceBetweenLines*0.75;
                 const xAxisRotation2=0;
@@ -131,7 +66,7 @@ $(function()
                 const sweepFlag2=1;
                 // End at D line
                 const finalX2 = x2;
-                const finalY2 = lines[1];
+                const finalY2 = staff.lines[1];
                 const path2=`M${x2},${y2} A${rx2},${ry2} ${xAxisRotation2} ${largeArcFlag2},${sweepFlag2} ${finalX2},${finalY2}`;
                 svg.path(path2).fill("none").stroke({width:STEM_STROKE, color: STEM_COLOUR});
                 // Start at same x
@@ -145,7 +80,7 @@ $(function()
                 const sweepFlag3=0;
                 // End at E line
                 const finalX3 = x2;
-                const finalY3 = lines[4];
+                const finalY3 = staff.lines[4];
                 const path3=`M${x3},${y3} A${rx3},${ry3} ${xAxisRotation3} ${largeArcFlag3},${sweepFlag3} ${finalX3},${finalY3}`;
                 svg.path(path3).fill("none").stroke({width:STEM_STROKE, color: STEM_COLOUR});
                 // Start at same x
@@ -159,13 +94,13 @@ $(function()
                 const sweepFlag4=0;
                 // End at E line
                 const finalX4 = x4;
-                const finalY4 = lines[2];
+                const finalY4 = staff.lines[2];
                 const path4=`M${x4},${y4} A${rx4},${ry4} ${xAxisRotation4} ${largeArcFlag4},${sweepFlag4} ${finalX4},${finalY4}`;
                 svg.path(path4).fill("none").stroke({width:STEM_STROKE, color: STEM_COLOUR});
                 // Start at same x
                 const x5=x;
                 // End at end of last line
-                const y5=lines[2];
+                const y5=staff.lines[2];
                 const rx5=CLEF_WIDTH*2/3;
                 const ry5=distanceBetweenLines/2;
                 const xAxisRotation5=0;
@@ -173,16 +108,101 @@ $(function()
                 const sweepFlag5=0;
                 // End at E line
                 const finalX5 = x;
-                const finalY5 = lines[3];
+                const finalY5 = staff.lines[3];
                 const path5=`M${x5},${y5} A${rx5},${ry5} ${xAxisRotation5} ${largeArcFlag5},${sweepFlag5} ${finalX5},${finalY5}`;
                 svg.path(path5).fill("none").stroke({width:STEM_STROKE, color: STEM_COLOUR});
                 break;
             case "bass":
+                // Not impletemented
                 break;
             case "none":
                 break;
             default:
                 console.warn("Clef "+clef+" not supported");
         }
+    }
+    /**
+     * Draws a note on the staff
+     */
+    function drawNote(svg, staff, type, x, spot)
+    {
+        switch(type)
+        {
+            case "quarter":
+                const ellipseHeight=10;
+                const ellipseWidth=15;
+
+                // SVG's move wants the top-left of the rectangle containing the ellipse
+                const ellipseLeft = staff.left + x - ellipseWidth/2;
+                const ellipseTop = staff.top + staff.height*(spot + 1/2)/9-staff.stroke/2-ellipseHeight/2;
+                svg.ellipse(ellipseWidth, ellipseHeight).move(ellipseLeft, ellipseTop);
+                // Draw the stem
+                if(spot<=4)
+                {
+                    // Draw stem pointing down
+                    // Stem starts at the left of the ellipse and goes down
+                    const stemTop = ellipseTop + ellipseHeight/2; // Vertical centre of the ellipse
+                    const stemBottom = ellipseTop + ellipseHeight/2 + STEM_HEIGHT;
+                    const stemX = ellipseLeft + ellipseWidth*.1; // Put the stem a little into the note so it isn't as obvious
+                    svg.line(stemX, stemTop, stemX, stemBottom).stroke({width:STEM_STROKE, color: STEM_COLOUR});
+                }
+                else
+                {
+                    // Draw stem pointing up
+                    // Stem starts at the right of the ellipse and goes up
+                    const stemTop = ellipseTop + ellipseHeight/2 - STEM_HEIGHT; // Vertical centre of the ellipse
+                    const stemBottom = ellipseTop + ellipseHeight/2;
+                    const stemX = ellipseLeft + ellipseWidth*.9; // Put the stem a little into the note so it isn't as obvious
+                    svg.line(stemX, stemTop, stemX, stemBottom).stroke({width:STEM_STROKE, color: STEM_COLOUR});
+                }
+                break;
+        }
+    }
+    /**
+     * Draws a staff
+     * @param {SVG} svg SVG.j(s object
+     * @param {number} left Left border of the staff
+     * @param {number} top Top border of the staff
+     * @param {int} width Width of the staff
+     * @param {int} height Height of the staff
+     * @param {int} stroke Line stroke of the staff
+     * @param {string} clef Type of clef to draw (treble/bass/none)
+     * @returns {object} {svg, left, top, width, height, stroke, clef, lines}
+     */
+    function drawStaff(svg, left, top, width, height, stroke, clef)
+    {
+        // Draw staff
+        // Save the line numbers so we can reference their positions later
+        const lines=[];
+        for(var i=0;i<=4;i++)
+        {
+            // Draw a line of the staff
+            // Go by 1/4.5th of height (plus 1/18th), so we start 1/18th from the top and end 1/18th from the bottom 
+            // We also need to subtract half of the stroke, so that the line is centred at the point we want it
+            lines.push(top+i*height/4.5+(height/18)-stroke/2);
+            svg.line(left, lines[i], left+width, lines[i]).stroke({width:stroke, color: STAFF_COLOUR});
+        }
+        const staff={svg, left, top, width, height, stroke, clef, lines};
+        svg.click(function(e)
+        {
+            // Get click coordinates relative to the svg
+            const clickCoordinates = localClickCoordinates(e);
+            console.log(clickCoordinates);
+            // Get coordinates relative to the staff
+            clickCoordinates.x-=left;
+            clickCoordinates.y-=top;
+            if(clickCoordinates.x<0 || clickCoordinates.x>width || clickCoordinates.y<0 || clickCoordinates.y>height)
+            {
+                // Ignore click, it was not meant for us
+                return;
+            }
+            // Calculate line/space on which we clicked
+            // Each line/space has a buffer of 1/9th of the staff, so multiply by 9 and get the floor
+            const clickedSpot=Math.floor(clickCoordinates.y/height*9);
+            // Place a quarter note at that spot
+            drawNote(svg, staff, "quarter", clickCoordinates.x, clickedSpot);
+        });
+        // Draw the clef
+        drawClef(svg, staff, clef);
     }
 });
